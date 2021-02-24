9a8be8b461dbbe89a498535370e304006014f4ce
Kairav Shah <kairavshah1702@gmail.com>
2021-02-24 17:47:14 +0530

    added support for patch request to update completed status

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:id).map { |todo| todo.to_pleasant_string }.join("\n")
   end
@@ -15,6 +17,23 @@ class TodosController < ApplicationController
   end
 
   def create
-    render plain: "#{params.to_s}"
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    response_text = "Created new todo with ID: #{new_todo.id}"
+    render plain: response_text
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "Updated todo #{id} completed status to #{completed}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "todos", to: "todos#index"
-  get "todos/make", to: "todos#create"
-  get "todos/:id", to: "todos#show"
+  # get "todos", to: "todos#index"
+  # get "todos/make", to: "todos#create"
+  # get "todos/:id", to: "todos#show"
+
+  resources :todos
 end
